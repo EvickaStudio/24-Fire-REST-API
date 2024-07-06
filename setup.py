7a66911c390efc1771e4adcb9d0c96d845bfbde3
EvickaStudio <me@evicka.de>
2024-07-06 23:20:44 +0200

    refactor(Setup): :recycle: Update fireapi package setup
    Updated the setup.py file to change the package version to 0.2 and the license to AGPL-3.0. Also updated the author email and the classifiers to reflect the new license.

--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@ from distutils.core import setup
 setup(
     name="fireapi",  # Package name
     packages=["fireapi"],  # Same as "name"
-    version="0.1",  # Start version
-    license="MIT",  # License type
+    version="0.2",  # Start version
+    license="AGPL-3.0",  # License type updated to AGPL-3.0
     description="A simple API wrapper for the 24Fire REST API",  # Short description
     author="EvickaStudio",  # Your name
-    author_email="kirenogfx@gmail.com",  # Your email
+    author_email="hello@evicka.de",  # Your email
     url="https://github.com/EvickaStudio/24-Fire-REST-API",  # GitHub repository URL
     keywords=["API", "24Fire", "KVM", "Server Management"],  # Keywords
     install_requires=[
         "requests",
     ],
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
     ],
 )
