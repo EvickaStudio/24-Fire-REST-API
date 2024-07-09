2cb264d894dfc652fa70954295daa8c6ce2bca40
EvickaStudio <me@evicka.de>
2024-07-09 21:16:14 +0200

    chore(Setup): :heavy_plus_sign: Update Setup
    Update fireapi package setup version to 0.3 and add requests dependency

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", encoding="utf-8") as f:
 setup(
     name="fireapi",
     packages=["fireapi"],
-    version="0.2",
+    version="0.3",
     license="AGPL-3.0",
     description="A simple API wrapper for the 24Fire REST API",
     long_description=long_description,  # Set the long description
@@ -17,7 +17,7 @@ setup(
     url="https://github.com/EvickaStudio/24-Fire-REST-API",
     keywords=["API", "24Fire", "KVM", "Server Management"],
     install_requires=[
-        "aiohttp",
+        "aiohttp", "requests"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
