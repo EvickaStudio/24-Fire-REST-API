1ad9f83276e45e8e739382e0417d3e91b7c728e4
EvickaStudio <hello@evicka.de>
2024-07-13 22:47:01 +0200

    feat(Interface): :sparkles: Add 24Fire+ endpoints
    Add 24Fire+ endpoints for monitoring and backup creation to the interface

--- fireapi/interface.py
+++ fireapi/interface.py
@@ -16,3 +16,18 @@ class IAPIOperations:
 
     def restart_server(self) -> Dict:
         pass
+
+    def incidences(self) -> Dict:
+        pass
+
+    def timings(self) -> Dict:
+        pass
+
+    def backup_list(self) -> Dict:
+        pass
+
+    def backup_create(self) -> Dict:
+        pass
+
+    def backup_delete(self) -> Dict:
+        pass
